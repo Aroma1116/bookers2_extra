68795e936a796a4733871ea2e89efad80216769b
Aroma1116 <aroma1116@outlook.jp>
2022-08-10 10:06:33 +0000

    実装開始

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,14 @@
+class FavoritesController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    @book = favorite.book
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+  end
+end
